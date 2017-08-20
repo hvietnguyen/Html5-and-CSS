3a7af79a4f34a90e08e2c38a7a7b04bb7b81cd4c
hvietnguyen <h.viet0384@gmail.com>
2017-08-20 16:38:10 +1200

    new commit

---
+++ Date&bootstrap/javascript/date.js
@@ -0,0 +1,53 @@
+$(function () {
+   var bindDatePicker = function() {
+	   // Call function to select date
+		$(".date").datetimepicker({
+        format:'DD-MM-YYYY',
+			icons: {
+				time: "fa fa-clock-o",
+				date: "fa fa-calendar",
+				up: "fa fa-arrow-up",
+				down: "fa fa-arrow-down"
+			}
+		}).find('input:first').on("focus",function () {
+			// check if the date is correct. We can accept dd-DD-yyyy and yyyy-DD-dd.
+			// update the format if it's yyyy-mm-dd
+			var date = parseDate($(this).val());
+
+			if (! isValidDate(date)) {
+				//create date based on momentjs (we have that)
+				date = moment().format('DD-MM-YYYY');
+			}
+
+			$(this).val(date);
+		});
+	}
+   
+   var isValidDate = function(value, format) {
+		format = format || false;
+		// lets parse the date to the best of our knowledge
+		if (format) {
+			value = parseDate(value);
+		}
+
+		var timestamp = Date.parse(value);
+
+		return isNaN(timestamp) == false;
+   }
+   
+   var parseDate = function(value) {
+	   debugger;
+		var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+		if (m)
+			value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+
+		return value;
+   }
+
+   bindDatePicker();
+ });
+
+ // Update value form date text input to label every 1000 milisecond
+ setInterval(function(){
+	$(".label-success").text("Date Selected: " + $("#date-text").val());
+ },1000);
